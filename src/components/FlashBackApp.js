46f9d0d44b211044ae15c913eb9aff557fe1b484
Claudwin <claudwin.fortune@gmail.com>
2019-03-04 12:08:25 -0700

    initial commit

---
+++ src/components/FlashBackApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FlashBackApp extends React.Component {
+  return 
+  render() {
+    return (
+      <div>
+      <h1>Hello React</h1>
+    </div>
+    )
+  
+  }
+}
+
+export default FlashBackApp;
