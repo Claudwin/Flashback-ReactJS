c2aefba40deb9d1b00a5a0749b0fc27e57d50163
Claudwin <claudwin.fortune@gmail.com>
2019-03-06 16:54:02 -0700

    remove react boilerplate

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import FlashBackApp from './components/FlashBackApp';
-
-ReactDOM.render(<FlashBackApp />, document.getElementById('app'));
-
